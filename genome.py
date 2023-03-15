1981c1fae686538b46a4970941fab42642138c14
Thomas Byr <tbouyer2000@gmail.com>
2023-03-15 16:51:52 +0100

    launch with `py genome.py`

--- genome.py
+++ genome.py
@@ -1,4 +1,6 @@
-from src import Tree
+import os
+
+from src import Tree, GenomeGUI
 
 from src.helper import info, debug
 
@@ -8,20 +10,36 @@ if __name__ == '__main__':
   plasmids = Tree('plasmids')
   prokaryotes = Tree('prokaryotes')
   viruses = Tree('viruses')
+  
+  # if "main_tree.json" exists in data directory, load it
+  # else, build the tree from scratch
+  should_build = False
+  main_tree: Tree = None
+  try:
+    os.open('data/main_tree.json', os.O_RDONLY)
+    main_tree = Tree.load('main_tree')
+    info(f'main tree loaded from file: {main_tree.filepath}')
+  except FileNotFoundError:
+    info('main tree not found, building from scratch')
+    should_build = True
 
-  eukaryotes.build()
-  info(f'eukaryotes built with {(n:=len(eukaryotes))} entr{"y" if n <= 1 else "ies"}')
-  plasmids.build()
-  info(f'plasmids built with {(n:=len(plasmids))} entr{"y" if n <= 1 else "ies"}')
-  prokaryotes.build()
-  info(f'prokaryotes built with {(n:=len(prokaryotes))} entr{"y" if n <= 1 else "ies"}')
-  viruses.build()
-  info(f'viruses built with {(n:=len(viruses))} entr{"y" if n <= 1 else "ies"}')
-  info()
+  if should_build:
+    eukaryotes.build()
+    info(f'eukaryotes built with {(n:=len(eukaryotes))} entr{"y" if n <= 1 else "ies"}')
+    plasmids.build()
+    info(f'plasmids built with {(n:=len(plasmids))} entr{"y" if n <= 1 else "ies"}')
+    prokaryotes.build()
+    info(f'prokaryotes built with {(n:=len(prokaryotes))} entr{"y" if n <= 1 else "ies"}')
+    viruses.build()
+    info(f'viruses built with {(n:=len(viruses))} entr{"y" if n <= 1 else "ies"}')
+    info()
 
-  main_tree = eukaryotes.union(plasmids).union(prokaryotes).union(viruses)
-  info(f'main tree built with {(n := len(main_tree))} kingdom{"" if n <= 1 else "s"}')
-  info(f'kingdoms: {", ".join(main_tree.tree.keys())}')
+    main_tree = eukaryotes.union(plasmids).union(prokaryotes).union(viruses)
+    info(f'main tree built with {(n := len(main_tree))} kingdom{"" if n <= 1 else "s"}')
+    info(f'kingdoms: {", ".join(main_tree.tree.keys())}')
 
-  main_tree.save('main_tree')
-  debug(f'wrote to file: {main_tree.filepath}')
+    main_tree.save('main_tree')
+    debug(f'wrote to file: {main_tree.filepath}')
+
+  gui = GenomeGUI(main_tree)
+  gui.run()
