b27a23a0db8f2b60ad960754731b2222f801eb87
ThomasByr <tbouyer2000@gmail.com>
2023-12-10 18:30:56 +0100

    ids are always retrieved online

--- src/core/tree.py
+++ src/core/tree.py
@@ -17,7 +17,7 @@ from ..helper import capture
 __all__ = ["Tree", "Value"]
 datetime_format = "%Y-%m-%d %H:%M:%S"
 non_valid_chars = string.punctuation + string.whitespace
-timeouts = (5, 10)
+timeouts = (5, 30)  # connect, read (seconds)
 
 
 @dataclass
@@ -32,21 +32,25 @@ class Tree:
 
     def __init__(self, name: str = None) -> None:
         """
-    Build a tree from a given name.\\
-    If no name is given, the tree will be built from the overview file.
+        Build a tree from a given name.\\
+        If no name is given, the tree will be built from the overview file.
 
-    ## Parameters
-    ```py
-    name : str, (optional)
-    ```
-    the name of the tree to build\\
-    defaults to `None`
-    """
+        ## Parameters
+        ```py
+        name : str, (optional)
+        ```
+        the name of the tree to build\\
+        defaults to `None`
+        """
         self.__name = "overview" if name is None else name
         self.__data: dict[str, Value] = {}
 
         load_dotenv()
         self.logger = logging.getLogger()
+        try:
+            os.mkdir("data")
+        except FileExistsError:
+            pass
 
         __rebuild = os.getenv("REBUILD")
         self.__is_rebuild: bool = False
@@ -62,21 +66,21 @@ class Tree:
 
     def build(self, force_rebuild: bool = False, silent: bool = False) -> None:
         """
-    Build the tree from the given name.\\
-    This method should only be called once.
+        Build the tree from the given name.\\
+        This method should only be called once.
 
-    ## Parameters
-    ```py
-    >>> force_rebuild : bool, (optional)
-    ```
-    force the tree to be rebuilt from https request\\
-    defaults to `False`
-    ```py
-    >>> silent : bool, (optional)
-    ```
-    suppress all excessively spamming output\\
-    defaults to `False`
-    """
+        ## Parameters
+        ```py
+        >>> force_rebuild : bool, (optional)
+        ```
+        force the tree to be rebuilt from https request\\
+        defaults to `False`
+        ```py
+        >>> silent : bool, (optional)
+        ```
+        suppress all excessively spamming output\\
+        defaults to `False`
+        """
         pickle_path = os.path.join("data", "tree.pkl")
         if not (force_rebuild or self.__is_rebuild) and os.path.exists(pickle_path):
             self.logger.info(f"loading tree ({pickle_path}) from pickle")
@@ -142,18 +146,35 @@ class Tree:
         valid_organisms: set[str] = set()
         if silent:
             capture.stop_redirect()
-        self.update_ids()
-        ids_files = os.listdir(os.path.join("data", "ids"))
+        # self.update_ids()
+        ids_files = [
+            "Archaea.ids",
+            "Bacteria.ids",
+            "dsDNA_Viruses.ids",
+            "Eukaryota.ids",
+            "Mito_metazoa.ids",
+            "Phages.ids",
+            "Plasmids.ids",
+            "Samples.ids",
+            "Viroids.ids",
+            "Viruses.ids",
+        ]
+        url = self.BASE_URL + "IDS/"
         for ids in ids_files:
-            self.logger.info(f"updating ids data from {ids}")
-            with open(os.path.join("data", "ids", ids), "r", encoding="utf-8") as f:
-                for line in f.readlines():
-                    row = line.split("\t")
-                    if not row[1].startswith("NC"):
-                        continue
-                    if (organism := re.sub(f"[{non_valid_chars}]", "_", row[5])) in self.__data:
-                        self.__data[organism].nc.add(row[1])
-                        valid_organisms.add(organism)
+            self.logger.info(f"getting ids data online from {ids}")
+            # get the file from the server
+            r = requests.get(url + ids, stream=True)
+            # if the file is not found
+            if r.status_code == 404:
+                self.logger.error(f"failed to get {ids} (file not found)")
+                continue
+            for line in r.iter_lines():
+                row = line.decode("utf-8").split("\t")
+                if not row[1].startswith("NC"):
+                    continue
+                if (organism := re.sub(f"[{non_valid_chars}]", "_", row[5])) in self.__data:
+                    self.__data[organism].nc.add(row[1])
+                    valid_organisms.add(organism)
 
         self.logger.info(f"found {len(valid_organisms)} valid organisms")
         self.clean_folders()
@@ -194,34 +215,34 @@ class Tree:
     @staticmethod
     def clean_folders() -> None:
         """
-    deletes all data from the `Results` directory\\
-    basically leaves the directory empty.
-    """
+        deletes all data from the `Results` directory\\
+        basically leaves the directory empty.
+        """
         for root, dirs, files in os.walk("Results", topdown=False):
             for name in files:
                 os.remove(os.path.join(root, name))
             for name in dirs:
                 os.rmdir(os.path.join(root, name))
 
-    def update_ids(self) -> None:
-        """
-    updates `data/ids` folder with the latest ids\\
-    this function will overwrite the existing files.
-    """
-        url = self.BASE_URL + "IDS/"
-        # for each file in the ids folder
-        for file in os.listdir("data/ids"):
-            name = file.split(".")[0]
-            # get the file from the server
-            r = requests.get(url + file, stream=True)
-            # if the file is not found
-            if r.status_code == 404:
-                self.logger.error(f"failed to update {name} (file not found)")
-                continue
-            # if the file is found
-            with open(f"data/ids/{file}", "wb") as f:
-                f.write(r.content)
-            self.logger.info(f"updated {file}")
+    # def update_ids(self) -> None:
+    #     """
+    #     updates `data/ids` folder with the latest ids\\
+    #     this function will overwrite the existing files.
+    #     """
+    #     url = self.BASE_URL + "IDS/"
+    #     # for each file in the ids folder
+    #     for file in os.listdir("data/ids"):
+    #         name = file.split(".")[0]
+    #         # get the file from the server
+    #         r = requests.get(url + file, stream=True)
+    #         # if the file is not found
+    #         if r.status_code == 404:
+    #             self.logger.error(f"failed to update {name} (file not found)")
+    #             continue
+    #         # if the file is found
+    #         with open(f"data/ids/{file}", "wb") as f:
+    #             f.write(r.content)
+    #         self.logger.info(f"updated {file}")
 
     def union(self, o: "Tree") -> "Tree":
         """
